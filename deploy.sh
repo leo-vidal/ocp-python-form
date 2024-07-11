613e56395ce8a0a1d08125b6134346d47dcbcf24
leocvidal <leo.vidal@au1.ibm.com>
2024-07-11 11:01:27 +1000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 oc delete bc ocp-python-form
 oc delete deployment ocp-python-form
 oc delete service ocp-python-form
+oc delete istag ocp-python-form:latest
 #oc new-app python:3.8~https://github.com/leocvidal/Basic-Form
 oc new-app https://github.com/leo-vidal/ocp-python-form --allow-missing-images
 oc expose service/ocp-python-form
