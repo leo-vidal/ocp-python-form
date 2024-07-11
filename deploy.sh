868515dfb4fdea11bbc9342d338172b1c00ae59f
leocvidal <leo.vidal@au1.ibm.com>
2024-07-11 11:01:02 +1000

    Dockerfile updated

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+oc delete bc ocp-python-form
+oc delete deployment ocp-python-form
+oc delete service ocp-python-form
+#oc new-app python:3.8~https://github.com/leocvidal/Basic-Form
+oc new-app https://github.com/leo-vidal/ocp-python-form --allow-missing-images
+oc expose service/ocp-python-form
